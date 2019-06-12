595a6fea62c03a3795aea22ded525c20bb52ba91
yinmo-public <yinmo.contact@gmail.com>
2019-06-12 11:21:32 +0800

    Update leave.py

--- leave.py
+++ leave.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from linepy import *
+from time import sleep
 yinmo = LINE()
 int1 = len(yinmo.getGroupIdsJoined())
 if int1 == 0:
@@ -7,6 +8,7 @@ if int1 == 0:
 else:
     for gid in yinmo.getGroupIdsJoined():
         print("已退出 " + yinmo.getGroup(gid).name)
+        sleep(0.5)
         yinmo.leaveGroup(gid)
     print("\n您退出" + str(int1) + "個群組")
     
